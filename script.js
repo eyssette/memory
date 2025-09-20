c8df71e7e33daddbf03323480f95459e337f7d53
Cédric Eyssette <cedric.eyssette@ac-lyon.fr>
2025-09-21 01:02:52 +0200

    chore: formattage JS et CSS

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Adaptation du Memory Game de Nate Wiley (License -- MIT / 2014)
 
-const backImage = "https://upload.wikimedia.org/wikipedia/commons/d/d6/Blue_Question_Circle.svg";
+const backImage =
+	"https://upload.wikimedia.org/wikipedia/commons/d/d6/Blue_Question_Circle.svg";
 
 const defaultMD = `
 # Memory
@@ -12,7 +13,7 @@ Hi !
 
 ![](https://forge.apps.education.fr/educajou/autobd/-/raw/main/contenus/personnages/Brigit_Komit/1/perso01.svg?ref_type=heads)
 Brigit & Komit
-`
+`;
 
 function handleURL(url, options) {
 	const corsProxy = "https://corsproxy.io/?url=";
@@ -27,7 +28,7 @@ function handleURL(url, options) {
 			addCorsProxy = false;
 			url = url.replace(
 				"https://github.com",
-				"https://raw.githubusercontent.com",
+				"https://raw.githubusercontent.com"
 			);
 			url = url.replace("/blob/", "/");
 		}
@@ -72,7 +73,7 @@ function getMarkdownFromURL(url, options) {
 		.catch((error) => {
 			console.error(
 				"There has been a problem with your fetch operation:",
-				error,
+				error
 			);
 			return defaultMD;
 		});
@@ -84,41 +85,38 @@ function getURLfromHash() {
 }
 
 function parseMarkdown(md) {
-	const content = md.replace(/#.*/g,"").replace(/\n\n+/g,'\n\n').trim()
-	const cardsContent = content.split("\n\n")
+	const content = md.replace(/#.*/g, "").replace(/\n\n+/g, "\n\n").trim();
+	const cardsContent = content.split("\n\n");
 	const cards = [];
 	cardsContent.forEach((card, index) => {
-		if(card.includes('\n')) {
-			const cardSubCards = card.split('\n')
-			cards.push({content:cardSubCards[0],id: index})
-			cards.push({content:cardSubCards[1],id: index})
+		if (card.includes("\n")) {
+			const cardSubCards = card.split("\n");
+			cards.push({ content: cardSubCards[0], id: index });
+			cards.push({ content: cardSubCards[1], id: index });
 		} else {
-			cards.push({content:card,id: index})
+			cards.push({ content: card, id: index });
 		}
 	});
-	return cards
+	return cards;
 }
 
-
 function duplicateUniqueCards(cards) {
-  // Compter les occurrences de chaque id
-  const counts = cards.reduce((acc, card) => {
-    acc[card.id] = (acc[card.id] || 0) + 1;
-    return acc;
-  }, {});
+	// Compter les occurrences de chaque id
+	const counts = cards.reduce((acc, card) => {
+		acc[card.id] = (acc[card.id] || 0) + 1;
+		return acc;
+	}, {});
 
-  // Filtrer les cartes uniques
-  const uniques = cards.filter(card => counts[card.id] === 1);
+	// Filtrer les cartes uniques
+	const uniques = cards.filter((card) => counts[card.id] === 1);
 
-  // Retourner le tableau original + duplication des uniques
-  return cards.concat(uniques);
+	// Retourner le tableau original + duplication des uniques
+	return cards.concat(uniques);
 }
 
 async function main() {
-	
 	var Memory = {
-		
-		init: function(cards){
+		init: function (cards) {
 			this.game = document.querySelector(".game");
 			this.modal = document.querySelector(".modal");
 			this.overlay = document.querySelector(".modal-overlay");
@@ -127,90 +125,103 @@ async function main() {
 			this.shuffleCards(this.cardsArray);
 			this.setup();
 		},
-		
-		shuffleCards: function(cardsArray){
+
+		shuffleCards: function (cardsArray) {
 			this.cards = this.shuffle(cardsArray);
 		},
-		
-		setup: function(){
+
+		setup: function () {
 			this.html = this.buildHTML();
 			this.game.innerHTML = this.html;
 			this.memoryCards = document.querySelectorAll(".card");
 			this.paused = false;
 			this.guess = null;
 			this.binding();
-			textFit(document.querySelectorAll('.front'), {multiLine: false});
+			textFit(document.querySelectorAll(".front"), { multiLine: false });
 		},
-		
-		binding: function(){
+
+		binding: function () {
 			var self = this;
-			this.memoryCards.forEach(function(card) {
-				card.addEventListener("click", function() {
+			this.memoryCards.forEach(function (card) {
+				card.addEventListener("click", function () {
 					self.cardClicked.call(self, card);
 				});
 			});
-			this.restartButton.addEventListener("click", function() {
+			this.restartButton.addEventListener("click", function () {
 				self.reset.call(self);
 			});
 		},
-		
-		cardClicked: function(card){
+
+		cardClicked: function (card) {
 			var $card = card;
 			var insideElement = $card.querySelector(".inside");
-			if(insideElement && !this.paused && !insideElement.classList.contains("matched") && !insideElement.classList.contains("picked")){
+			if (
+				insideElement &&
+				!this.paused &&
+				!insideElement.classList.contains("matched") &&
+				!insideElement.classList.contains("picked")
+			) {
 				insideElement.classList.add("picked");
-				if(!this.guess){
+				if (!this.guess) {
 					this.guess = $card.getAttribute("data-id");
-				} else if(this.guess == $card.getAttribute("data-id") && !$card.classList.contains("picked")){
-					document.querySelectorAll(".picked").forEach(function(pickedCard) {
-							pickedCard.classList.add("matched");
+				} else if (
+					this.guess == $card.getAttribute("data-id") &&
+					!$card.classList.contains("picked")
+				) {
+					document.querySelectorAll(".picked").forEach(function (pickedCard) {
+						pickedCard.classList.add("matched");
 					});
 					this.guess = null;
 				} else {
 					this.guess = null;
 					this.paused = true;
 					var self = this;
-					setTimeout(function(){
-						document.querySelectorAll(".picked").forEach(function(pickedCard) {
-								pickedCard.classList.remove("picked");
+					setTimeout(function () {
+						document.querySelectorAll(".picked").forEach(function (pickedCard) {
+							pickedCard.classList.remove("picked");
 						});
 						self.paused = false;
 					}, 600);
 				}
-				if(document.querySelectorAll(".matched").length == document.querySelectorAll(".card").length){
+				if (
+					document.querySelectorAll(".matched").length ==
+					document.querySelectorAll(".card").length
+				) {
 					this.win();
 				}
 			}
 		},
-		
-		win: function(){
+
+		win: function () {
 			this.paused = true;
 			var self = this;
-			setTimeout(function(){
+			setTimeout(function () {
 				self.showModal();
 			}, 1000);
 		},
-		
-		showModal: function(){
+
+		showModal: function () {
 			this.overlay.style.display = "block";
 			this.modal.style.display = "block";
 		},
-		
-		hideModal: function(){
+
+		hideModal: function () {
 			this.overlay.style.display = "none";
 			this.modal.style.display = "none";
 		},
-		
-		reset: function(){
+
+		reset: function () {
 			this.hideModal();
 			this.shuffleCards(this.cardsArray);
 			this.setup();
 			this.game.style.display = "block";
 		},
-		
+
 		// Fisher--Yates Algorithm -- https://bost.ocks.org/mike/shuffle/
-		shuffle: function(array){
-			var counter = array.length, temp, index;
+		shuffle: function (array) {
+			var counter = array.length,
+				temp,
+				index;
 			// While there are elements in the array
 			while (counter > 0) {
 				// Pick a random index
@@ -224,25 +235,25 @@ async function main() {
 			}
 			return array;
 		},
-		
-		buildHTML: function(){
-			var frag = '';
 
-			this.cards.forEach(function(card) {
-				const cardContent = marked.parseInline(card.content)
-				frag += 
-				`<div class="card" data-id="${card.id}"><div class="inside"><div class="front">${cardContent}</div><div class="back"><img src="${backImage}" alt="" /></div></div></div>`;
+		buildHTML: function () {
+			var frag = "";
+
+			this.cards.forEach(function (card) {
+				const cardContent = marked.parseInline(card.content);
+				frag += `<div class="card" data-id="${card.id}"><div class="inside"><div class="front">${cardContent}</div><div class="back"><img src="${backImage}" alt="" /></div></div></div>`;
 			});
 			return frag;
-		}
+		},
 	};
 
-	const md = getURLfromHash() ? await getMarkdownFromURL(getURLfromHash()) : defaultMD
+	const md = getURLfromHash()
+		? await getMarkdownFromURL(getURLfromHash())
+		: defaultMD;
 
-	const cards = parseMarkdown(md)
+	const cards = parseMarkdown(md);
 
 	Memory.init(cards);
-
-};
+}
 
 main();
